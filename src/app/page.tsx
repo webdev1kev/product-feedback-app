98c1bac3b9eae80aa425b18e811ac89904011138
Kevin Davis <kevindavis@Kevins-MacBook-Pro.local>
2023-10-24 23:25:59 -0500

    add intial styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,1 +1,10 @@
-export default function Home() {}
+import BackIcon from "@/assets/back-icon.svg";
+
+export default function Home() {
+  return (
+    <main>
+      <h1>Sed egestas ante et vulputate volutpat</h1>
+      <button className="button button-back">Button 1</button>
+    </main>
+  );
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,7 +8,23 @@ const config: Config = {
     "./globals.css",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        purple: "#AD1FEA",
+        blue: "#4661E6",
+        lightBlue: "#62BCFA",
+        darkBlue: "#373F68",
+        offDarkBlue: "#3A4374",
+        grayBlue: "#647196",
+        gray: "#F2F4FF",
+        lightGray: "#F7F8FD",
+        orange: "#F49F85",
+        purpleHover: "#C75AF6",
+        blueHover: "#7C91F9",
+        darkBlueHover: "#656EA3",
+        orangeHover: "#E98888",
+      },
+    },
   },
   plugins: [],
 };
